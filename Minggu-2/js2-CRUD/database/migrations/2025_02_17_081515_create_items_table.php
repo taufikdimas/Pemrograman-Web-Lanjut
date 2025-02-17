e5ef0feb58b864a9947851e9f87e50fe8cca4a5a
taufikdimas <taufikdimasedystara@gmail.com>
2025-02-17 15:21:09 +0700

    add:migrations

--- Minggu-2/js2-CRUD/database/migrations/2025_02_17_081515_create_items_table.php
+++ Minggu-2/js2-CRUD/database/migrations/2025_02_17_081515_create_items_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
+
         });
     }
 
