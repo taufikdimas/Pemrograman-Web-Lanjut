e94834e069a655c9c4dc137287327d080954ca9a
taufikdimas <taufikdimasedystara@gmail.com>
2025-02-17 15:55:43 +0700

    fix : item model

--- Minggu-2/js2-CRUD/app/Models/item.php
+++ Minggu-2/js2-CRUD/app/Models/item.php
@@ -4,7 +4,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class item extends Model
+class Item extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+    ];
 }
