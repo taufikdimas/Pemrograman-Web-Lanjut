3725a92aaf35ee251b171d5e228b480cb78da87e
taufikdimas <taufikdimasedystara@gmail.com>
2025-03-08 22:55:30 +0700

    feat: JS4 - Pratikum 2.5 Attribute Changes

--- PWL-POS/app/Http/Controllers/UserController.php
+++ PWL-POS/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -65,8 +66,49 @@ class UserController extends Controller
         // );
         // $user->save();
 
+        // JS4: Praktimum 2.5
+        // $user = UserModel::create([
+        //     'username' => 'manager27',
+        //     'nama'     => 'Manager27',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+        // $user->username = 'manager27';
+
+        // $user->isDirty();                     // true
+        // $user->isDirty('username');           // true
+        // $user->isDirty('nama');               // false
+        // $user->isDirty(['nama', 'username']); // true
+
+        // $user->isClean();                     // false
+        // $user->isClean('username');           // false
+        // $user->isClean('nama');               // true
+        // $user->isClean(['nama', 'username']); // false
+
+        // $user->save();
+
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+        // dd($user->isDirty());
+
+        $user = UserModel::create([
+            'username' => 'manager200',
+            'nama'     => 'Manager200',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+        $user->username = 'manager8';
+
+        $user->save();
+
+        $user->wasChanged();                         // true
+        $user->wasChanged('username');               // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama');                   // false
+        dd($user->wasChanged(['nama', 'username'])); // true
+
         //akses user model
-        $user = UserModel::all();
+        // $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
