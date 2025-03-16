d55d1697d8e85e9deb0b55c8ec734e9f129785ca
taufikdimas <taufikdimasedystara@gmail.com>
2025-03-16 20:31:46 +0700

    feat: implementasi JS5

--- PWL-POS/app/Http/Controllers/KategoriController.php
+++ PWL-POS/app/Http/Controllers/KategoriController.php
@@ -1,27 +1,73 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
+use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
-        // $data = [
-        //     'kategori_kode' => 'SNK',
-        //     'kategori_nama' => 'Snack/Makanan Ringan',
-        //     'created_at'    => now(),
-        // ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
+        return $dataTable->render('kategori.index');
+    }
 
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+    public function create()
+    {
+        return view('kategori.create');
+    }
 
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
+    }
 
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+    // public function edit($id)
+    // {
+    //     $data = KategoriModel::findOrFail($id);
+    //     return view('kategori.edit', compact('data'));
+    // }
+
+    // public function update(Request $request, $id)
+    // {
+    //     $kategori = KategoriModel::findOrFail($id);
+    //     $kategori->update([
+    //         'kategori_kode' => $request->kodeKategori,
+    //         'kategori_nama' => $request->namaKategori,
+    //     ]);
+
+    //     return redirect('/kategori');
+    // }
+
+    public function edit($id)
+    {
+        $kategori = KategoriModel::findOrFail($id);
+        return view('kategori.edit', compact('kategori'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'kodeKategori' => 'required|max:255',
+            'namaKategori' => 'required|max:255',
+        ]);
+
+        $kategori = KategoriModel::findOrFail($id);
+        $kategori->update([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+
+        return redirect()->route('kategori.index')->with('success', 'Kategori berhasil diperbarui');
+    }
+    public function delete($id)
+    {
+        $row = KategoriModel::findOrFail($id);
+        $row->delete();
+        return redirect('/kategori');
     }
 }
